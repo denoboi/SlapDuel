fbf47fc971bb5ae273f3655d2789e8223c439bef
denoboi <deniz.kaymakci@hotmail.com>
2022-06-07 17:43:28 +0300

    new TPose Enemy + HealthBar.cs

---
+++ Assets/[SlapDuel]/Scripts/Runtime/CharacterScripts/HealthBar.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private Image _healthBar;
+    public float CurrentHealthPlayer;
+    public float CurrentHealthEnemy;
+    private float _maxHealth = 100f;
+
+    private PlayerController _playerController;
+    private AIController _aiController;
+
+    public PlayerController PlayerController { get { return _playerController == null ? _playerController = GetComponentInParent<PlayerController>() : _playerController; } }
+    public AIController AIController { get { return _aiController == null ? _aiController = GetComponentInParent<AIController>() : _aiController; } }
+
+
+    private void Start()
+    {
+       _healthBar = GetComponent<Image>();
+        
+        
+
+    }
+
+    private void Update()
+    {
+
+        CurrentHealthPlayer = PlayerController.Health.CurrentHealth;
+        _healthBar.fillAmount = CurrentHealthPlayer / _maxHealth;
+       
+    }
+
+
+
+}
